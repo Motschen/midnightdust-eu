c580b9e181a6eba826108bf683f2d8df2b8e3580
Martin Prokoph <motschen@midnightdust.eu>
2025-07-30 23:22:41 +0200

    feat: improve styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,27 @@ import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 
 import alpinejs from "@astrojs/alpinejs";
+import rehypeCitation from 'rehype-citation';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.midnightdust.eu',
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false
-  }), alpinejs()],
+  integrations: [
+    mdx(
+      {rehypePlugins: [[rehypeCitation, {
+        bibliography: "https://raw.githubusercontent.com/timlrx/rehype-citation/main/test/references-data.bib",
+        linkCitations: true,
+        showTooltips: true,
+        inlineClass: ["citation"],
+        csl: "https://raw.githubusercontent.com/citation-style-language/styles/master/acm-sig-proceedings.csl"
+      }]]}
+    ),
+    icon(),
+    tailwind({
+      applyBaseStyles: false
+    }
+  ), alpinejs()],
   vite: {
     css: {
         preprocessorOptions: {
